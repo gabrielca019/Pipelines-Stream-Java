c598877a4bc801c26263864a723f066db68f34e5
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2023-08-14 11:14:22 -0300

    Construtor, getters, setters e toString da classe Funcionario

---
+++ src/Funcionario.java
@@ -0,0 +1,44 @@
+
+public class Funcionario {
+	
+	private String nome;
+	private String email;
+	private double salario;
+	
+	public Funcionario(String nome, String email, double salario) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.salario = salario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + nome + ", Email: " + email + ", Salario: " + salario;
+	}
+	
+}
