6c470af53ef66df9d12be240c51e9a86b11546df
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2023-08-14 11:17:42 -0300

    Criação da lista de funcionarios

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		List<Funcionario> listaFuncionarios = Arrays.asList(
+				new Funcionario("Gabriel", "gabriel@gmail.com", 1551.0),
+				new Funcionario("Paulo", "paulo@gmail.com", 2000.0),
+				new Funcionario("Matheus", "matheus@gmail.com", 3000.0),
+				new Funcionario("Jackson", "jack@gmail.com", 500.0),
+				new Funcionario("França", "frnc@gmail.com", 10000.0),
+				new Funcionario("Rafael", "rafa@gmail.com", 1550.0));
 	}
 
 }
